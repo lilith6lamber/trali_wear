9b7aa76a5f0c22d7001c873369a4bf618d477a61
Lilith Lamber <winter.rituel@gmail.com>
2021-09-16 18:57:58 +0300

    + loading screen

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
+'use strict';
+const wait = (delay = 0) =>
+    new Promise(resolve => setTimeout(resolve, delay));
+
+const setVisible = (selector, visible) => {
+    selector = document.getElementById(selector);
+    if (visible) {
+        selector.classList.add("visible");
+        document.documentElement.style.overflowY = 'hidden';
+    } else {
+        selector.classList.remove("visible");
+        document.body.classList.add("visible");
+        document.documentElement.style.overflowY = 'visible';
+    }
+}
+
+setVisible('loadingScreen', true);
+
+document.addEventListener('DOMContentLoaded', () =>
+    wait(1000).then(() => {
+        setVisible('loadingScreen', false);
+    }));
+
+document.addEventListener("DOMContentLoaded", () =>  {
     const lazyLoadInstance = new LazyLoad();
     drawNavbar();
     drawSlider();
